bdb6ea4fb8bf417ad44845f1521bb45eaa0d5e4e
Ilham Satya Nusabhakti <ilhamsn2021@gmail.com>
2025-12-28 20:40:40 +0700

    chore: cleanup

--- test/BaseTest.t.sol
+++ test/BaseTest.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../src/FLECFactory.sol";
-import "./utils/MockToken.sol"; // Pastikan kamu punya file MockToken sederhana
+import "./utils/MockToken.sol"; 
 
 abstract contract BaseTest is Test {
     FLECFactory public factory;
